7db2e2843d9a9159cd63d04e505e68d3530d8412
BradleyParkerDev <bradleyscottparker@outlook.com>
2022-09-21 11:45:01 -0500

    Updated

---
+++ make-divisble.js
@@ -0,0 +1,19 @@
+const prompt = require('prompt-sync')({sigint:true});
+
+console.log("Enter x: ");
+let x = Number(prompt('> '));
+
+console.log("Enter y: ");
+let y = Number(prompt('> '));
+//let x2 = x;
+
+
+while(x % y !== 0){
+    
+    x+=1;    
+        
+}
+
+
+console.log(`${x} is divisible by ${y}`)
+
